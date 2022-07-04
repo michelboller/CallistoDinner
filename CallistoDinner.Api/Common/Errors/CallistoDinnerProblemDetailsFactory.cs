8dc4d68aca0cdbddb93f7443285276307f76b383
Michel Ticiani <michel.ticiani@appsistemas.com.br>
2022-07-04 01:02:42 -0300

    Moving some files into the correct folders

---
+++ CallistoDinner.Api/Common/Errors/CallistoDinnerProblemDetailsFactory.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace CallistoDinner.Api.Common.Errors
+{
+    public class CallistoDinnerProblemDetailsFactory : ProblemDetailsFactory
+    {
+        private readonly ApiBehaviorOptions _options;
+        public CallistoDinnerProblemDetailsFactory(IOptions<ApiBehaviorOptions> options)
+        {
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public override ProblemDetails CreateProblemDetails(HttpContext httpContext, int? statusCode = null, string? title = null, string? type = null, string? detail = null, string? instance = null)
+        {
+            statusCode ??= 500;
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = title,
+                Type = type,
+                Detail = detail,
+                Instance = instance
+            };
+
+            ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+            return problemDetails;
+        }
+
+        public override ValidationProblemDetails CreateValidationProblemDetails(HttpContext httpContext, ModelStateDictionary modelStateDictionary, int? statusCode = null, string? title = null, string? type = null, string? detail = null, string? instance = null)
+        {
+            statusCode ??= 500;
+
+            var problemDetails = new ValidationProblemDetails
+            {
+                Status = statusCode,
+                Title = title,
+                Type = type,
+                Detail = detail,
+                Instance = instance
+            };
+
+            if (title != null)
+            {
+                problemDetails.Title = title;
+            }
+
+            ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+            return problemDetails;
+        }
+
+        private void ApplyProblemDetailsDefaults(HttpContext httpContext, ProblemDetails problemDetails, int statusCode)
+        {
+            /*
+             * The error could be logged into a file and the traceId used to get more infomation about the given error.
+             * To the end user is returned on RFC-7807 standard but for the developer is logged more infos (for example the stacktrace)
+             */
+
+            problemDetails.Status ??= statusCode;
+
+            if (_options.ClientErrorMapping.TryGetValue(statusCode, out var clientErrorData))
+            {
+                problemDetails.Title ??= clientErrorData.Title;
+                problemDetails.Type ??= clientErrorData.Link;
+            }
+
+            var traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
+            if (traceId != null)
+            {
+                problemDetails.Extensions["traceId"] = traceId;
+            }
+
+            var exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            var exceptionType = exception?.GetType();
+            problemDetails.Extensions.Add("ExceptionType", exceptionType?.Name ?? "Unknown");
+        }
+
+    }
+}
